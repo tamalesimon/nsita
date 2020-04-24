84c758943683d77bd0e0014ae2a1ecad73103f2b
tsp <tamalesp@gmail.com>
2020-04-25 01:10:53 +0300

    updated navigation menu-item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const HatsPage = () => (
+
+  <div><h1>HATS PAGE</h1></div>
+)
+
+const ShirtsPage = () => (
+
+  <div><h1>Shirt Pages</h1></div>
+)
+
 function App() {
   return (
-   <div className='App'>
-     <HomePage/>
+   <div>
+   <Switch>
+     <Route exact path='/' component={HomePage}></Route>
+     <Route path='/hats' component={HatsPage}></Route>
+     <Route path='/shirts' component={ShirtsPage}></Route>
+   </Switch>
+     
    </div>
   );
 }
