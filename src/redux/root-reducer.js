189e968b3837941890d15fa281d0c1a7aaa450ee
tamalesimon <tamalesp@gmail.com>
2020-05-12 02:17:07 +0300

    updating cart item with a memoize selector

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,9 @@
-import {combineReducers} from 'redux';
-import userReducer from       './user/user.reducer';
+import { combineReducers } from 'redux';
+
+import userReducer from './user/user.reducer.js';
 import cartReducer from './cart/cart.reducer';
 
-
 export default combineReducers({
-    user: userReducer,
-    cart: cartReducer
+  user: userReducer,
+  cart: cartReducer
 });
